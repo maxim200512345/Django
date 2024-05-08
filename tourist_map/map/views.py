3c932d5584339460a8c9c8a68e5fcf1574570c7f
maxim200512345 <kedrov.maksim2005@gmail.com>
2024-05-08 10:27:49 +0500

    fixedBackEnd

--- tourist_map/map/views.py
+++ tourist_map/map/views.py
@@ -10,7 +10,7 @@ class SomeView(TemplateView):
     template_name = 'index.html'
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['data'] = [
+        context['all'] = [
             {
                 'id' : obj.id,
                 'headline' : obj.headline,
