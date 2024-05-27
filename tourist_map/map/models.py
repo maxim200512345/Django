f054e59ff659c22d05cbc40a8549edc5a127ccd2
maxim200512345 <kedrov.maksim2005@gmail.com>
2024-05-27 14:46:58 +0500

    add an array with coords for lines

--- tourist_map/map/models.py
+++ tourist_map/map/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django_mysql.models import ListCharField
 class TouristObject(models.Model):
     headline = models.CharField(max_length=20)
     description = models.TextField()
@@ -14,3 +14,9 @@ class TouristObject(models.Model):
     link_to_map = models.TextField()
     approximate_duration = models.TextField()
     distance_from_railwayStation = models.IntegerField()
+    array = ListCharField(
+        base_field=models.CharField(max_length=25),
+        size=35,
+        max_length=(35 * 26)
+    )
+
