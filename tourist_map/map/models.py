7310edbb2d3f13a646a5e9241b3d7db7d682fad7
maxim200512345 <kedrov.maksim2005@gmail.com>
2024-05-27 14:59:37 +0500

    remove unusefull values from db

--- tourist_map/map/models.py
+++ tourist_map/map/models.py
@@ -6,17 +6,7 @@ class TouristObject(models.Model):
     xCoords = models.TextField()
     yCoords = models.TextField()
     location = models.TextField()
-    difficult = models.IntegerField()
-    is_for_kids = models.BooleanField()
-    approximate_budget = models.IntegerField()
-    start_coordinates = models.TextField()
-    end_coordinates = models.TextField()
     link_to_map = models.TextField()
-    approximate_duration = models.TextField()
-    distance_from_railwayStation = models.IntegerField()
-    array = ListCharField(
-        base_field=models.CharField(max_length=25),
-        size=35,
-        max_length=(35 * 26)
-    )
+    detailedDescription = models.TextField()
+    typeOfRoute = models.TextField()
 
